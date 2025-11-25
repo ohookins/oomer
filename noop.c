271df69e185135bae7395a8c91dae48354b9b4b8
Oliver Hookins <ohookins@gmail.com>
2025-11-25 12:15:09 +1100

    Didn't commit all the noop.c changes yet.

--- noop.c
+++ noop.c
@@ -2,18 +2,21 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void signal_handler(int signum) {
+void signal_handler(int signum)
+{
     printf("\nCaught signal %d, exiting gracefully.\n", signum);
     exit(0);
 }
 
-int main() {
+int main()
+{
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
 
     printf("noop container started. Doing nothing forever.\n");
     // Sleep indefinitely
-    while (1) {
+    while (1)
+    {
         sleep(86400); // Sleep for a day
     }
     return 0;
